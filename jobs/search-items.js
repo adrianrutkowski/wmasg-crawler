f0b03d64e8e43781fe3fe97f6f6de4f4a1da1705
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2017-08-24 18:48:29 +0200

    Implement filtering items by keywords and price and passing result back

--- jobs/search-items.js
+++ jobs/search-items.js
@@ -6,12 +6,23 @@ class SearchItems {
     this.concurrency = concurrency;
   }
 
+  match(item, expression) {
+    return item.title.toLowerCase().match(expression) || item.description.toLowerCase().match(expression);
+  }
+
   process(job, done) {
     const spider = new WMASGSpider();
+    const {keywords, price} = job.data;
+    const expression = new RegExp(`(${keywords.join('|')})`, 'g');
+    const items = [];
 
     spider.on('error', error => done(error));
-    spider.on('end', () => done());
-    spider.on('item', item => console.log(item));
+    spider.on('end', () => done(null, items));
+    spider.on('item', item => {
+      if ((!price || item.price <= price) && this.match(item, expression)) {
+        items.push(item);
+      }
+    });
 
     spider.start();
   }
